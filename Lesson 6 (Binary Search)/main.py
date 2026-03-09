fbd569190951c3a70f32aa5923bafa1f8ada5932
aarav245 <aaravmisra24@gmail.com>
2026-03-08 19:26:46 -0700

    Add files via upload

---
+++ Lesson 6 (Binary Search)/main.py
@@ -0,0 +1,6 @@
+list1 = []
+user1 = int(input("How many terms would you like to add?"))
+for i in range(user1):
+    user2 = int(input("What would you like to add? (Use increasing order) "))
+    list1.append(user2)
+print(list1)
