e84dc4700b365ce726cc49b6b38b05838d5a9071
aarav245 <aaravmisra24@gmail.com>
2026-03-16 19:08:32 -0700

    Add files via upload

---
+++ Lesson 7 (Bubble sort)/main2.py
@@ -0,0 +1,13 @@
+list1 = []
+size = int(input("What size do you want the list to be? "))
+for i in range(size):
+    elements = input("What would you like to add? ")
+    list1.append(elements)
+print(list1)
+for s in range(size):
+    for k in range(0,size-s-1):
+        if list1[k] > list1[k+1]:
+            temp = list1[k]
+            list1[k] = list1[k+1]
+            list1[k+1] = temp
+print(list1)
